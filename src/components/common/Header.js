891d45f53cda136474b6a540ed12aa31fbbd947b
Lehi Arteaga <jose.arteaga@unosquare.com>
2019-07-04 14:13:10 -0500

    add reset button

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,18 +1,30 @@
 import React, { Fragment } from 'react'
+import { withRouter } from 'react-router-dom'
 import '../../styles/Header.scss'
 
+const ResetButton = withRouter(({ history }) => (
+  <div role='img' alt='Today'
+    className='reset-btn' onClick={() => { history.push('/') }}
+  >
+    <div className='icon'>
+      today
+    </div>
+  </div>
+))
+
 const AppHeader = () => {
-    return (
-      <Fragment>
-        <header className='header'>
-          <nav className='title'>
-            <span>
-              Netflix<b> Originals</b>
-            </span>
-          </nav>
-        </header>
-      </Fragment>
-    )
-  }
-  
-  export default AppHeader;
+  return (
+    <Fragment>
+      <header className='header'>
+        <nav className='title'>
+          <ResetButton />
+          <span>
+            Netflix<b> Originals</b>
+          </span>
+        </nav>
+      </header>
+    </Fragment>
+  )
+}
+
+export default AppHeader;
