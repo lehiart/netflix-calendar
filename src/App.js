b3c50dd4672583c852f546d343fa9d195a3de856
Lehi Arteaga <jose.arteaga@unosquare.com>
2019-07-04 12:11:41 -0500

    create sections

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import './styles/App.scss'
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className='App'>
+      <header className='header'>
+        <nav className='title'>
+          <span>
+            Netflix<b> Originals</b>
+          </span>
+        </nav>
+      </header>
+      <main>
+        <div className='calendar'>
+          Calendar content
+        </div>
+      </main>
+      <footer>
+        <nav className='title'>
+          <span>SEE WHAT'S NEXT</span>
+        </nav>
+      </footer>
     </div>
   );
 }
